b3d2e53cccaf3458157f9dea5270c641d2687b3a
Andreas Goss <agoss@itemis.com>
2024-02-15 15:07:49 +0100

    Refactor component into three separate components

--- part1/couseinfo/src/App.jsx
+++ part1/couseinfo/src/App.jsx
@@ -1,10 +1,40 @@
+const Header = ({title }) => {
+  return (
+    <h1>{title}</h1>
+  )
+}
 
-function App() {
+const Content = ({part, exercise}) => {
+  return (
+    <p>
+      {part} {exercise}
+    </p>
+  )
+}
+
+const Total = ({total}) => {
+  return (
+    <p>Number of exercises {total}</p>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
 
   return (
-    <>
-      Hello World
-    </>
+    <div>
+      <Header title={course}/> 
+      <Content part={part1} exercise={exercises1}/>
+      <Content part={part2} exercise={exercises2}/>
+      <Content part={part3} exercise={exercises3}/>
+      <Total total={exercises1 + exercises2 + exercises3}/>
+    </div>
   )
 }
 
