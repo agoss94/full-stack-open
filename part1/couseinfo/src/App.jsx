8c602ef1343a407475562b52c028a093a3ce92ea
Andreas Goss <agoss@itemis.com>
2024-02-16 07:23:35 +0100

    Exercise 1.5

--- part1/couseinfo/src/App.jsx
+++ part1/couseinfo/src/App.jsx
@@ -1,4 +1,4 @@
-const Header = ({title }) => {
+const Header = ({ title }) => {
   return (
     <h1>{title}</h1>
   )
@@ -14,7 +14,7 @@ const Content = (props) => {
   )
 }
 
-const Part = ({name, exercises}) => {
+const Part = ({ name, exercises }) => {
   return (
     <p>
       {name} {exercises}
@@ -22,32 +22,34 @@ const Part = ({name, exercises}) => {
   )
 }
 
-const Total = ({parts}) => {
+const Total = ({ parts }) => {
   return (
     <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
   )
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [{
-    name: 'Fundamentals of React',
-    exercises: 10
-  },
-  {
-    name: 'Using props to pass data',
-    exercises: 7
-  },
-  {
-    name: 'State of a component',
-    exercises: 14
-  }]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [{
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }]
+  }
 
   return (
     <div>
-      <Header title={course}/> 
-      <Content parts={parts}/>
-      <Total parts={parts}/>
+      <Header title={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
