ede3fcbed6ce518825cea6bb2fdb4b782d72420f
Andreas Goss <agoss@itemis.com>
2024-02-22 14:55:06 +0100

    Exercise 2.6

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -6,19 +6,29 @@ const App = () => {
   ]) 
   const [newName, setNewName] = useState('')
 
+  const saveNumber = (e) => {
+    e.preventDefault()
+    setPersons(persons.concat({name: newName}))
+    setNewName('')
+  }
+
+  const handleInputChange = (e) => {
+    setNewName(e.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleInputChange}/>
         </div>
         <div>
-          <button type="submit">add</button>
+          <button type="submit" onClick={saveNumber}>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map(person => <p key={person.name}>{person.name}</p>)}
     </div>
   )
 }
