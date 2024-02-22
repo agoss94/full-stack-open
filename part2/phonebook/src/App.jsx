d55212ecf7527f4db8558a0161a0c14f8b7da513
Andreas Goss <agoss@itemis.com>
2024-02-22 16:00:14 +0100

    Exercise 2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,40 +1,57 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [newName, setNewName] = useState('')
+    {
+      name: "Arto Hellas",
+      number: "040-1234567"
+    },
+  ]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [filter, setNewFilter] = useState('')
 
   const saveNumber = (e) => {
-    e.preventDefault()
-    if (persons.filter(p => p.name === newName).length > 0) {
-      alert(`${newName} is already added to the phonebook`)
-      return
+    e.preventDefault();
+    if (persons.filter((p) => p.name === newName).length > 0) {
+      alert(`${newName} is already added to the phonebook`);
+      return;
     }
-    setPersons(persons.concat({name: newName}))
-    setNewName('')
-  }
+    setPersons(persons.concat({ name: newName, number: newNumber }));
+    setNewName("");
+    setNewNumber("")
+  };
 
-  const handleInputChange = (e) => {
-    setNewName(e.target.value)
-  }
+  const personsToShow = filter.length === 0 ? persons : persons.filter(p => p.name.toLowerCase().indexOf(filter.toLowerCase()) !== -1)
 
   return (
     <div>
+      filter: <input value={filter} onChange={e => setNewFilter(e.target.value)}/>
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input value={newName} onChange={handleInputChange}/>
+          name:{" "}
+          <input value={newName} onChange={(e) => setNewName(e.target.value)} />
         </div>
         <div>
-          <button type="submit" onClick={saveNumber}>add</button>
+          number:{" "}
+          <input
+            value={newNumber}
+            onChange={(e) => setNewNumber(e.target.value)}
+          />
+        </div>
+        <div>
+          <button type="submit" onClick={saveNumber}>
+            add
+          </button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <p key={person.name}>{person.name}</p>)}
+      {personsToShow.map((person) => (
+        <p key={person.name}>{person.name} {person.number}</p>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
