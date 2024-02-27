6a6ac1fa8742efb01c85d50d13c3206d75c0f8cb
Andreas Goss <agoss@itemis.com>
2024-02-27 11:04:10 +0100

    change importance of notes

--- part2/notes/src/App.jsx
+++ part2/notes/src/App.jsx
@@ -31,6 +31,14 @@ const App = () => {
     setNewNote(event.target.value)
   }
 
+  const toggleImportanceOf = (id) => {
+    const note = notes.find(n => n.id === id)
+    const changedNote = {...note, important: !note.important}
+    noteService.update(id, changedNote).then(response => {
+      setNotes(notes.map(note => note.id !== id ? note : response.data))
+    })
+  }
+
   const notesToShow = showAll ? notes : notes.filter(note => note.important)
 
   return (
@@ -43,7 +51,7 @@ const App = () => {
       </div>
       <ul>
         {notesToShow.map(note => 
-          <Note key={note.id} note={note} />
+          <Note key={note.id} note={note} toggleImportance={() => toggleImportanceOf(note.id)}/>
         )}
       </ul>
       <form onSubmit={addNote}>
