2f6a77fbabc5519f3816d5cedb787dd9833b7ba9
Andreas Goss <agoss@itemis.com>
2024-02-28 10:27:27 +0100

    Exercise 2.12

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,56 +1,64 @@
 import { useState, useEffect } from "react";
-import axios from 'axios'
-
-const Filter = ({value, onChange}) => {
+import service from "./services/service";
+const Filter = ({ value, onChange }) => {
   return (
     <>
-      filter: <input value={value} onChange={onChange}/>
+      filter: <input value={value} onChange={onChange} />
     </>
-  )
-}
+  );
+};
 
-const PersonForm = ({newName, setNewName, newNumber, setNewNumber, onSubmit}) => {
-  return (<form>
-        <div>
-          name:{" "}
-          <input value={newName} onChange={(e) => setNewName(e.target.value)} />
-        </div>
-        <div>
-          number:{" "}
-          <input
-            value={newNumber}
-            onChange={(e) => setNewNumber(e.target.value)}
-          />
-        </div>
-        <div>
-          <button type="submit" onClick={onSubmit}>
-            add
-          </button>
-        </div>
-      </form>)
-}
+const PersonForm = ({
+  newName,
+  setNewName,
+  newNumber,
+  setNewNumber,
+  onSubmit,
+}) => {
+  return (
+    <form>
+      <div>
+        name:{" "}
+        <input value={newName} onChange={(e) => setNewName(e.target.value)} />
+      </div>
+      <div>
+        number:{" "}
+        <input
+          value={newNumber}
+          onChange={(e) => setNewNumber(e.target.value)}
+        />
+      </div>
+      <div>
+        <button type="submit" onClick={onSubmit}>
+          add
+        </button>
+      </div>
+    </form>
+  );
+};
 
-const Persons = ({person}) => {
+const Persons = ({ person }) => {
   return (
     <>
       {person.map((person) => (
-        <p key={person.name}>{person.name} {person.number}</p>
+        <p key={person.name}>
+          {person.name} {person.number}
+        </p>
       ))}
     </>
-  )
-}
+  );
+};
 
 const App = () => {
   const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
-  const [filter, setNewFilter] = useState('')
+  const [filter, setNewFilter] = useState("");
   useEffect(() => {
-    axios.get('http://localhost:3001/persons').then(response => {
-      const persons = response.data
-      setPersons(persons)
-    })
-  }, [])
+    service.getAll().then((initialPersons) => {
+      setPersons(initialPersons);
+    });
+  }, []);
 
   const saveNumber = (e) => {
     e.preventDefault();
@@ -58,20 +66,35 @@ const App = () => {
       alert(`${newName} is already added to the phonebook`);
       return;
     }
-    setPersons(persons.concat({ name: newName, number: newNumber }));
+    service
+      .create({ name: newName, number: newNumber })
+      .then((returnedPerson) => {
+        setPersons(persons.concat(returnedPerson));
+      });
     setNewName("");
-    setNewNumber("")
+    setNewNumber("");
   };
 
-  const personsToShow = filter.length === 0 ? persons : persons.filter(p => p.name.toLowerCase().indexOf(filter.toLowerCase()) !== -1)
+  const personsToShow =
+    filter.length === 0
+      ? persons
+      : persons.filter(
+          (p) => p.name.toLowerCase().indexOf(filter.toLowerCase()) !== -1
+        );
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <Filter value={filter} onChange={e => setNewFilter(e.target.value)}/>
-      <PersonForm newName={newName} setNewName={setNewName} newNumber={newNumber} setNewNumber={setNewNumber} onSubmit={saveNumber}/>
+      <Filter value={filter} onChange={(e) => setNewFilter(e.target.value)} />
+      <PersonForm
+        newName={newName}
+        setNewName={setNewName}
+        newNumber={newNumber}
+        setNewNumber={setNewNumber}
+        onSubmit={saveNumber}
+      />
       <h2>Numbers</h2>
-      <Persons person={personsToShow}/>
+      <Persons person={personsToShow} />
     </div>
   );
 };
